e61ef6f0cd7421ca2523dab5a0c9e2d87dd76727
sweethuman <gheo@sweethuman.tech>
2021-12-25 03:38:27 +0200

    feat: add home page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
+import { Helmet } from "react-helmet";
 
 const Layout: React.FC = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Helmet>
+        <title>Album Cards</title>
+        <meta
+          name="description"
+          content="Print out a physical version of a digital album to put in your collection. Scan and play."
+        />
+      </Helmet>
+      {children}
+    </>
+  );
 };
 
 export default Layout;
